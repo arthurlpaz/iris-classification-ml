6787a0350252f71f32645024163481b4f3067abf
arthurlpaz <arthurpcristovao@gmail.com>
2026-04-09 14:27:33 -0300

    done: pipeline to run model built

---
+++ src/pipelines/training_pipeline.py
@@ -0,0 +1,34 @@
+from sklearn.model_selection import train_test_split, cross_val_score
+from src.data.load_data import load_data
+from src.data.preprocess import preprocess_data
+from src.models.train_model import train_model
+from src.models.evaluate import evaluate_model
+from src.models.save_model import save_model
+import yaml
+
+def run_pipeline():
+    configs = yaml.safe_load(open("configs/config.yaml"))
+
+    df = load_data()
+    X, y = preprocess_data(df)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=configs["data"]["test_size"],
+        random_state=configs["data"]["random_state"]
+    )
+
+    model = train_model(X_train, y_train, configs)
+
+    cv_scores = cross_val_score(model, X, y, cv=5)
+
+    print(f"CV Accuracy Mean: {cv_scores.mean():.4f}")
+    print(f"CV Accuracy Std: {cv_scores.std():.4f}")
+    accuracy = evaluate_model(model, X_test, y_test)
+
+    print(f"Model Accuracy: {accuracy:.4f}")
+    print(f"Cross-Validation Scores: {cv_scores}")
+
+    save_model(model)
+
+
---
+++ train.py
@@ -0,0 +1,5 @@
+from src.pipelines.training_pipeline import run_pipeline
+
+if __name__ == "__main__":
+    run_pipeline()
+    
