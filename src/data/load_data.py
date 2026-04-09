ba8ff0473087cc9c8ecac199838c9f20fa17f4e9
arthurlpaz <arthurpcristovao@gmail.com>
2026-04-09 14:17:56 -0300

    fix: loading data corectly

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -2,7 +2,7 @@ from sklearn.datasets import load_iris
 import pandas as pd
 
 def load_data():
-    data = load_iris()
+    data = load_iris(as_frame=True)
     df = data.frame
     df['target'] = data.target
     return df
